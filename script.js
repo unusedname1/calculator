1223c2a37469ce0e382be93f09235c201821d744
futurebcdev <zordevoanon@protonmail.com>
2022-02-05 09:52:49 -0500

    finished ui

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+func add(a, b)
+    return a + b
+
+func subtract(a, b)
+    return a - b
+
+func multiply(a, b)
+    return a * b
+
+func divide(a, b)
+    return a / b
+
+func operate(operator, a, b):
+    switch operator:
+        +:
+            add(a, b)
+        -:
+            subtract(a, b)
+        *:
+            multiply(a, b)
+        /:
+            divide(a, b)
+        def:
+            throw unknown
+
+func onClick():
+    switch:
+        operator:
+            operate();
+        number:
+        clear:
+        backspace:
+
+
+
+*/
