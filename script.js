401c2f59a97771162453256501d8e3cefe90dde4
futurebcdev <zordevoanon@protonmail.com>
2022-02-08 21:40:33 -0500

    finished calculator

--- script.js
+++ script.js
@@ -1,37 +1,166 @@
-/*
-func add(a, b)
-    return a + b
+const ROUND_DIGIT = 9;
+const DIGIT_LIMIT = 11;
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.btn');
+const inputScr = document.querySelector('.input');
+const equationScr = document.querySelector('.equation');
+let inputText = '0';
+let obtainCommand = {
+    'period': '.',
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'backspace': 'BKSP',
+    'sign': 'SIGN',
+    'percent': 'PCNT',
+    'clear': 'CLR',
+    'multiply': '×',
+    'divide': '÷',
+    'add': '+',
+    'subtract': '-',
+    'equal': '=',
+}
+let equation = {
+    operands: [],
+    operator: '',
+    process: function (number, newOperator) {
+        let operands = this.operands;
+        let oldOperator = this.operator;
+        let a, b, ans;
 
-func subtract(a, b)
-    return a - b
+        if (number) {
+            if (number.endsWith('.'))
+                number = number.slice(0, -1);
+            operands.push(number);
+        }
 
-func multiply(a, b)
-    return a * b
+        if (operands.length === 2) {
+            a = operands[0];
+            b = operands[1];
+            ans = operate(a, b, oldOperator);
+            this.operands = [ans];
 
-func divide(a, b)
-    return a / b
+            if(ans.length > DIGIT_LIMIT) {
+                clearAll();
+                return;
+            }
+        }
 
-func operate(operator, a, b):
-    switch operator:
-        +:
-            add(a, b)
-        -:
-            subtract(a, b)
-        *:
-            multiply(a, b)
-        /:
-            divide(a, b)
-        def:
-            throw unknown
+        if (ans && newOperator === '=') {
+            inputText = ans;
+            display(equationScr, a + oldOperator + b + newOperator);
+            this.operator = newOperator;
+            this.operands = [];
+            return true;
+        }
+        else if (newOperator !== '=') {
+            display(equationScr, this.operands[0] + newOperator);
+            this.operator = newOperator;
+        }
 
-func onClick():
-    switch:
-        operator:
-            operate();
-        number:
-        clear:
-        backspace:
+        inputText = '';
+        return false;
+    },
+    clear: function () {
+        display(equationScr, '');
+        this.operands = [];
+        this.operator = '';
+    },
+}
 
+init();
 
+function init() {
+    display(inputScr, inputText);
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', onBtnClick);
+    });
+    body.addEventListener
+}
 
-*/
+function onBtnClick(event) {
+    const id = event.currentTarget.id;
+    processInput(obtainCommand[id]);
+}
+
+function processInput(str) {
+    // is it just me who thinks 
+    // that having break statement in default looks more organized?
+    switch (str) {
+        case '×':
+        case '÷':
+        case '-':
+        case '+':
+        case '=':
+            if (equation.process(inputText, str))
+                display(inputScr, inputText);
+            break;
+        case '.':
+            if (!inputText.includes('.'))
+                inputText += '.';
+            display(inputScr, inputText);
+            break;
+        case 'BKSP':
+            if (inputText !== '0')
+                inputText = inputText.slice(0, -1);
+            if (inputText === '-' || inputText === '')
+                inputText = '0';
+            display(inputScr, inputText);
+            break;
+        case 'SIGN':
+            if (inputText.startsWith('-'))
+                inputText = inputText.slice(1);
+            else
+                inputText = '-' + inputText;
+            if(inputText === '-')
+                inputText = '-0';
+            display(inputScr, inputText);
+            break;
+        case 'PCNT':
+            inputText = operate(inputText, '100', '÷');
+            display(inputScr, inputText);
+            break;
+        case 'CLR':
+            clearAll();
+            break;
+        default:
+            if (inputText === '0') inputText = '';
+            else if (inputText === '-0') inputText = '-';
+            inputText += str;
+            display(inputScr, inputText);
+            break;
+    }
+    if(inputText.length > DIGIT_LIMIT) clearAll();
+}
+
+function display(screen, text) {
+    screen.textContent = text;
+}
+
+function clearAll() {
+    inputText = '0';
+    display(inputScr, inputText);
+    equation.clear();
+}
+
+function operate(a, b, operator) {
+    let ans;
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '×': ans = a * b; break;
+        case '÷': ans = a / b; break;
+        case '+': ans = a + b; break;
+        case '-': ans = a - b; break;
+        default: throw 'unrecognizable operator';
+    }
+    ans = +ans.toFixed(ROUND_DIGIT);
+    return ans.toString();
+}
